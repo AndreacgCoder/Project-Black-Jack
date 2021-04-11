aeadfe442325f8e1f8a43f2f026fe03df34637a3
Andrea Comabasosa Garrido <comabasosa.g.a@gmail.com>
2021-04-11 12:45:56 +0200

    2nd commit

--- src/Modulo_de_directorios/Cards.js
+++ src/Modulo_de_directorios/Cards.js
@@ -1,9 +1,21 @@
 /* Definimos la classe de naipes, donde cada objecto representa una carta*/
-
-class Card {
-    constructor (card_type,card_text, card_value) {
+var a;
+var b;
+export class Card {
+    constructor (card_type, card_color,card_text, card_value) {
         this.type = card_type;
+        this.color = card_color;
         this.text = card_text;
         this.value = card_value;
     }
 }
+
+var CardList = { 0: new Card("Heart", "Red", 'A', 1), 1: new Card("Heart", "Red", '2', 2)};
+
+function FirstCards (){
+    const a = Math.floor(Math.random()*1);
+    console.log(a);
+    let cards = [CardList[0],CardList[1]];
+    return cards;
+}
+console.log(FirstCards());
--- src/main.js
+++ src/main.js
@@ -1,7 +1,50 @@
+import {Card} from './Modulo_de_directorios/Cards.js';
 /**
 Juego de cartas BlackJack
  **/
 
- 
+main()
+
+function main(){
+    document.getElementById("quit").addEventListener("click", quit);
+    document.getElementById("hit").addEventListener("click", hit);
+    let cardList = JSON.parse(cards.json);
+    var firstCards = FirstCards(cardList);
+    console.log('firstCards = ' + firstCards);
+    console.log(document.getElementById("hand"));//.update(firstCards);
+    document.getElementById("hand").innerHTML = document.getElementById("hand").innerHTML + firstCards[0].text;
+    
+}
+
+function hit(cardsHand){
+    updateHand(card);
+    cardsHand.push(newCard());
+    
+}
+
+function quit(){
+    
+    updateTable(card);
+}
+
+function updateTable(card){
+    document.getElementById("table").innerHTML = document.getElementById("table").innerHTML + card.text;
+}
+
+function updateHand(card){
+    document.getElementById("hand").innerHTML = document.getElementById("hand").innerHTML + card.text;
+}
+
+function FirstCards (){
+    console.log(a, b);
+    let cardsHand = [newCard(a), newCard(b)];
+    return cardsHand;
+}
+
+function newCard (cardList){
+    const a = Math.floor(Math.random()*51);
+    let card = [cardList[a]];
+    return card;
+}
 
 
