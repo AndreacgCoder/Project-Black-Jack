c066fff44924590cdff72cf785915d4d79ba77ae
Andrea Comabasosa Garrido <comabasosa.g.a@gmail.com>
2021-04-17 15:43:21 +0200

    commit cards

--- src/index.js
+++ src/index.js
@@ -2,75 +2,137 @@
 Juego de cartas BlackJack
  **/
 import Card from './cards.js';
-import * as puntuation from './puntuation';
 
 var cardslist = [];
 var handplayer = [];
 var handbank = [];
+var valueplayer = 0;
+var valuebank = 0;
 
 
 document.getElementById("restart").addEventListener("click", restart);
 document.getElementById("stay").addEventListener("click", stay);
 document.getElementById("hit").addEventListener("click", hit);
+/*var card = new Card ("heart", "red", "A", [1,11], 0);*/
+/*updateHand(card);*/
 startgame();
-//scoregame();
+updateBothValues();
 
 
 function startgame(){
     cardslist = newdeck();
     handplayer = firstcards(cardslist);
     handbank = firstcards(cardslist);
+    calculateValues();
     console.log(document.getElementById("bank"));
     console.log(document.getElementById("player"));
     document.getElementById("player").innerHTML = document.getElementById("player").innerHTML + handplayer[0].text + " " + handplayer[1].text;
     document.getElementById("bank").innerHTML = document.getElementById("bank").innerHTML + handbank[0].text + " " + handbank[1].text;
+    document.getElementById("player").innerHTML = document.getElementById("player").innerHTML + newCardPrint(handplayer[0]);
+    document.getElementById("player").innerHTML = document.getElementById("player").innerHTML + newCardPrint(handplayer[1]); 
+    document.getElementById("bank").innerHTML = document.getElementById("bank").innerHTML + newCardPrint(handbank[0]);
+    document.getElementById("bank").innerHTML = document.getElementById("bank").innerHTML + newCardPrint(handbank[1]);   
+}
+
+function calculateValues(){
+    valueplayer = 0;
+    handplayer.forEach(cardInHand => valueplayer = valueplayer + cardInHand.value);
+    valuebank = 0;
+    handbank.forEach(cardInHand => valuebank = valuebank + cardInHand.value);
 }
 
 function hit(){
     var card = newCard();
+    if (card.number === 0 || card.number === 26 || card.number===39 || card.number === 13) {
+        card.value = 11;
+        calculateValues();
+        if (valueplayer > 21 ){
+            card.value = 1;
+        }
+    }
     handplayer.push(card);
     updateHand(card); 
     updateValue('player');
+    if (valueplayer>21) {
+        stay();
+    }
 }
 
 function updateValue(player){
-    var value = 0;
+    calculateValues();
     if (player == 'player'){
-        handplayer.forEach(cardInHand => value = value + cardInHand.value);
-        document.getElementById("playerValue").innerHTML = "<h2>" + value + "</h2>";
+        document.getElementById("playerValue").innerHTML = "<h2>" + valueplayer + "</h2>";
     } else {
-        handplayer.forEach(cardInHand => value = value + cardInHand.value);
-        document.getElementById("bankValue").innerHTML = "<h2>" + value + "</h2>";
+        document.getElementById("bankValue").innerHTML = "<h2>" + valuebank + "</h2>";
     }
 }
 
-function scoregame(){
-    scorebank();
-}
-
-function scorebank() {
-    if(handbank[0].value + handbank[1].value >=17) {
-        stay(bank);
-    } else{
-        hit(handbank);
-    }
-}
 
 function restart(){
     handplayer = [];
     handbank = [];
+    valuebank = 0;
+    valueplayer = 0;
+    document.getElementById("player").innerHTML = "";
+    document.getElementById("bank").innerHTML = " ";
+    document.getElementById("stay").disabled = false;
+    document.getElementById("hit").disabled = false;
     startgame();
+    updateBothValues();
+    document.getElementById("resum").innerHTML = "";
 }
 
-function stay(hand) {
-    if (hand === handbank){
-        document.getElementById("bank").innerHTML = "Puntuación banca:"; 
+function stay() {
+    var valuebank = 0;
+    handbank.forEach(cardInHand => valuebank = valuebank + cardInHand.value);
+    while (valuebank < 17){
+        var card = newCard();
+        if (card.number === 0 || card.number === 26 || card.number===39 || card.number === 13) {
+            card.value = 11;
+            if (valueplayer > 21 ){
+                card.value = 1;
+            }
+        }
+        handbank.push(card);
+        handbank.forEach(cardInHand => valuebank = valuebank + cardInHand.value);
+            updateHandBank(card);
+            updateValue('bank');
+    }
+    result();
+}
+function result(){
+    document.getElementById("stay").disabled = true;
+    document.getElementById("hit").disabled = true;
+    if(valueplayer > 21 && valuebank > 21){
+        document.getElementById("resum").innerHTML = "It's a draw";
+    } else if ((valuebank < 21 && valueplayer > 21) || (valuebank < 21 && valueplayer < valuebank)){
+        document.getElementById("resum").innerHTML = "Has perdido!";
+    } else {
+        document.getElementById("resum").innerHTML = "Has ganado!";
     }
 }
 
 function updateHand(card){
     console.log(card);
-    document.getElementById("player").innerHTML = document.getElementById("player").innerHTML + " " + card.text;
+    //document.getElementById("player").innerHTML = document.getElementById("player").innerHTML + " " + card.text;
+
+    document.getElementById("player").innerHTML = document.getElementById("player").innerHTML + newCardPrint(card);
+}
+
+function newCardPrint(card){
+    return "<img src=\"" + "./imagenes/" + card.number + ".png" + "\" ></img>";
+    /*" + card.text + "\" ></img>";*/
+}
+
+function updateHandBank(card){
+    console.log(card);
+    document.getElementById("bank").innerHTML = document.getElementById("bank").innerHTML + " " + card.text;
+    document.getElementById("bank").innerHTML = document.getElementById("bank").innerHTML + newCardPrint(card);
+}
+
+function updateBothValues(){
+    updateValue('player');
+    updateValue('bank');
 }
 
 function firstcards (){
@@ -87,6 +149,9 @@ function newCard (){
     console.log(a);
     var card = cardslist[a];
     console.log(card);
+    if (card.number === 0 || card.number === 26 || card.number===39 || card.number === 13) {
+        card.value = 11;
+    }
     cardslist.splice(a, 1);
     printDECK();
     return card;
@@ -99,7 +164,7 @@ function printDECK() {
 
 function newdeck (){
 
-    var cardslist = [new Card ("heart", "red", "A", 1, 0),
+    var cardslist = [new Card ("heart", "red", "A", [1,11], 0),
     new Card ("heart", "red", "2", 2, 1),
     new Card ("heart", "red", "3", 3, 2),
     new Card ("heart", "red", "4", 4, 3),
